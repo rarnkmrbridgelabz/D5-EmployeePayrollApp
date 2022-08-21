8cd095020c91cff4df3316ccf1b52c2d7d7a0a0b
Arun <rarnkmrbridgelabz.com>
2022-08-21 11:20:08 +0530

    Adding UC-2

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.employeepayrollapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollApplication {
+
+    public static void main(String[] args) {
+       ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+       log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+    }
+
+}
