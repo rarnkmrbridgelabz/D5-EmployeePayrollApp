55780e846ae094b4994bc28fd9e51b6a0f94df55
Arun <rarnkmrbridgelabz.com>
2022-08-25 11:14:01 +0530

    Ensuring validations are done on Payroll DTO

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -3,8 +3,7 @@ package com.bridgelabz.employeepayrollapp.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,13 +14,17 @@ public @ToString class EmployeePayrollDTO {
     public String name;
     @Min(value =500,message = "Min wage should be more than 500")
     public  long salary;
-
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
-
     @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start Date should not be Empty")
+    @PastOrPresent(message = "Start Date should be past or today's date")
     public LocalDate startDate;
+    @NotBlank(message = "Note cannot be Empty")
     public String note;
+    @NotBlank(message = "Profile Pic cannot be Empty")
     public String profilePic;
+    @NotNull(message = "Departments should not be Empty")
     public List<String> departments;
 
 }
